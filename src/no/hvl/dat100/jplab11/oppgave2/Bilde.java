85dfb5829b59c08dae8b9fcfbdcf86888f93f638
TobiasManthey <tobias.manthey321@gmail.com>
2023-11-19 14:05:41 +0100

    oblig3

--- src/no/hvl/dat100/jplab11/oppgave2/Bilde.java
+++ src/no/hvl/dat100/jplab11/oppgave2/Bilde.java
@@ -4,35 +4,36 @@ import no.hvl.dat100.jplab11.common.TODO;
 
 public class Bilde extends Tekst {
 
-	// TODO - objekt variable
+	private String url;
 	
 	public Bilde(int id, String bruker, String dato, String tekst, String url) {
-		throw new UnsupportedOperationException(TODO.constructor("Bilde"));
+		super(id,bruker,dato,tekst);
+		this.url = url;
 	}
 
 	public Bilde(int id, String bruker, String dato, int likes, String tekst, String url) {
-		throw new UnsupportedOperationException(TODO.constructor("Bilde"));
+		super(id,bruker,dato,likes,tekst);
+		this.url = url;
 	}
 	
 	public String getUrl() {
-		throw new UnsupportedOperationException(TODO.method());
+		return url;
 
 	}
 
 	public void setUrl(String url) {
-		throw new UnsupportedOperationException(TODO.method());
+		this.url = url;
 	}
 
 	@Override
 	public String toString() {
-		throw new UnsupportedOperationException(TODO.method ());
-
+		return "BILDE\n" + getId() + "\n" + getBruker() + "\n" + getDato() + "\n" + getLikes() + "\n" + getTekst() + "\n" + url + "\n";
 	}
 
 	// Metoden nedenfor er kun for valgfri oppgave 6
 	public String toHTML() {
 		
-		throw new UnsupportedOperationException(TODO.method());
+		return "<p>BILDE</p><p>ID: " + getId() + "</p><p>Bruker: " + getBruker() + "</p><p>Dato: " + getDato() + "</p><p>Likes: " + getLikes() + "</p><p>" + getTekst() + "</p><img src='" + url + "'><br>";
 				
 	}
 }
--- src/no/hvl/dat100/jplab11/oppgave4/SkrivBlogg.java
+++ src/no/hvl/dat100/jplab11/oppgave4/SkrivBlogg.java
@@ -8,8 +8,32 @@ import no.hvl.dat100.jplab11.oppgave3.*;
 
 public class SkrivBlogg {
 
-	public static boolean skriv(Blogg samling, String mappe, String filnavn) {
+    public static boolean skriv(Blogg samling, String mappe, String filnavn) {
+        try (PrintWriter writer = new PrintWriter(mappe + "/" + filnavn)) {
+            writer.print(samling.toString());
+            return true;
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+            return false;
+        }
+    }
 
-		throw new UnsupportedOperationException(TODO.method());
-	}
+    public static void main(String[] args) {
+ 
+        Blogg blogg = new Blogg();
+       
+
+      
+        String mappe = "/Users/tobiasmanthey/Documents/test";
+        String filnavn = "blogg.dat";
+
+      
+        boolean success = skriv(blogg, mappe, filnavn);
+
+        if (success) {
+            System.out.println("Blogg written to file successfully.");
+        } else {
+            System.out.println("Failed to write blogg to file.");
+        }
+    }
 }
